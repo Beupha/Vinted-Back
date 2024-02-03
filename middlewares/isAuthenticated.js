eb6c9c9163c4eb7a8490f2d8f114abe161d6e8f9
Beupha <fabien.stodel@laposte.net>
2024-02-03 16:17:04 +0100

    correction picture offer

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,11 +1,11 @@
 const User = require("../models/User");
 
 const isAuthenticated = async (req, res, next) => {
-  console.log(req.headers.authorization);
+  // console.log("Bearer tok -->", req.headers.authorization);
   const user = await User.findOne({
     token: req.headers.authorization.replace("Bearer ", ""),
   });
-
+  console.log("user -->", user);
   if (user) {
     req.user = user;
 
